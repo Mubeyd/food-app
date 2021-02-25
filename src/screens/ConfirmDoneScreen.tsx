82f3a5f29ce9f4f3a83456e87b106f156e79c728
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2021-02-25 16:45:44 +0300

    adding lottie packages

--- src/screens/ConfirmDoneScreen.tsx
+++ src/screens/ConfirmDoneScreen.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { windowWidth } from '../config/styleConstants';
 
 const ConfirmDoneScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text style={styles.headerText}>ConfirmDoneScreen</Text>
+        <View style={styles.contanier}>
+            <View style={styles.headerAnimation}>
+                <LottieView
+                    source={require('../assets/animations/8881-wave.json')}
+                    autoPlay
+                    loop
+                />
+            </View>
+            <View style={styles.centerAnimation}>
+                <LottieView
+                    source={require('../assets/animations/676-done.json')}
+                    autoPlay
+                    loop
+                />
+            </View>
+            {/* <Text style={styles.headerText}>ConfirmDoneScreen</Text> */}
             <TouchableOpacity
                 onPress={() => navigation.navigate('HomeScreen')}
                 style={styles.button}>
@@ -17,6 +33,20 @@ const ConfirmDoneScreen = ({ navigation }) => {
 export default ConfirmDoneScreen;
 
 const styles = StyleSheet.create({
+    contanier: {
+        flexDirection: 'column',
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerAnimation: {
+        height: 200,
+        width: 400,
+    },
+    centerAnimation: {
+        height: 300,
+        width: windowWidth,
+    },
     headerText: {
         fontSize: 44,
         fontWeight: 'bold',
